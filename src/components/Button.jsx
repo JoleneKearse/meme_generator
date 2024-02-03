33f23e662c408927e8c012b7eaccc8b333a8f01b
JoleneKearse <pharaohnutz@gmail.com>
2024-02-03 17:36:25 -0500

    chore: adjust button margin

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import img from "/image.svg"
 
 const Button = () => {
   return (
-    <button className="w-full col-span-2 rounded-md flex justify-center items-center gap-2 p-3 bg-gradient-to-r from-blue-900 to-zinc-900 text-lg hover:bg-zinc-800 hover:text-cyan-500  md:text-xl">
+    <button className="w-full col-span-2 rounded-md flex justify-center items-center gap-2 p-3 mt-5 bg-gradient-to-r from-blue-900 to-zinc-900 text-lg hover:bg-zinc-800 hover:text-cyan-500  md:text-xl">
       Get a new Meme image
       <img src={img} alt="meme canvas" className="w-8 hover:animate-spin" />
     </button>
