aada0e6b9f9bee5df6e3c6f7f59bfc88909c60f1
JoleneKearse <pharaohnutz@gmail.com>
2024-02-04 11:06:04 -0500

    chore: update props

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,8 +5,18 @@ const Form = () => {
   return (
     <form className='font-mono text-stone-200 text-xl'>
       <div className="grid ">
-        <Input labelName="Top Text" />
-        <Input labelName="Bottom Text" />
+        <Input 
+          labelName={"Top Text"}
+          placeholder={"Top Text"}
+          id={"top"}
+          htmlFor={"top"}
+                  />
+        <Input 
+          labelName={"Bottom Text"}
+          placeholder={"Bottom Text"}
+          id={"bottom"}
+          htmlFor={"bottom"}
+        />
       </div>
       <Button />
     </form >
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,7 @@
-const Input = ({ labelName, placeholder }) => (
+const Input = ({ labelName, placeholder, id, htmlFor }) => (
   <div className="py-4 min-w-full flex flex-col">
     <label
-      htmlFor="top"
+      htmlFor={htmlFor}
       className="text-stone-200 block pb-3 text-lg md:text-center lg:text-left"
     >
       {labelName}
@@ -9,7 +9,7 @@ const Input = ({ labelName, placeholder }) => (
     <input
       type="text"
       name="top"
-      id="topText"
+      id={id}
       placeholder={placeholder}
       className="rounded-md border-slate-950 bg-zinc-400 border-2 text-stone-100 text-lg p-2" />
   </div>
